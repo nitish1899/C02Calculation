8c313b733e9a61d1956fa5fba3a6b37602f41743
nitish1899 <nitishcyberminds@gmail.com>
2024-08-07 13:21:14 +0530

    add forgot pin

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,4 @@
-const User = require("../models/user");
+const userModel = require("../models/user");
 const bcrypt = require("bcrypt");
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
@@ -16,7 +16,7 @@ async function register(req, res) {
     const hashPin = await bcrypt.hash(pin.toString(), 10);
 
     // Find and update the user if they exist, or create a new user if they don't
-    const newUser = await User.findOneAndUpdate(
+    const newUser = await userModel.findOneAndUpdate(
       { mobileNumber },  // Find user by mobile number
       { userName, pin: hashPin }, // Update or set these fields
       { new: true, upsert: true, setDefaultsOnInsert: true } // Options: return the new doc if one is upserted
@@ -58,7 +58,7 @@ async function register(req, res) {
 async function login(req, res) {
   try {
     const { mobileNumber, pin } = req.body;
-    const existingUser = await User.findOne({ mobileNumber });
+    const existingUser = await userModel.findOne({ mobileNumber });
     // console.log('existingUser', existingUser)
     if (!existingUser) {
       // user is not registered yet need to register yourself.
@@ -104,4 +104,41 @@ async function login(req, res) {
   }
 }
 
-module.exports = { register, login };
+async function forgotPin(req, res) {
+  try {
+    const { mobileNumber, pin, confirmPin } = req.body;
+
+    // Check if user exists
+    const existingUser = await userModel.findOne({ mobileNumber });
+
+    if (!existingUser) {
+      return res.status(400).json({ status: 'failed', msg: "User doesn't exist" });
+    }
+
+    // Check if pin and confirmPin match
+    if (pin !== confirmPin) {
+      return res.status(400).json({ status: 'failed', msg: 'Pin and confirmPin must be the same' });
+    }
+
+    // Hash the new pin
+    const hashPin = await bcrypt.hash(pin.toString(), 10);
+
+    // Update the user's pin
+    await userModel.findOneAndUpdate(
+      { mobileNumber }, // Query to find the user
+      { $set: { pin: hashPin } }, // Update to apply
+      { new: true } // Options: return the updated document
+    );
+
+    // Respond with success message
+    res.status(200).json({ status: 'success', msg: 'Pin has been updated successfully' });
+  } catch (error) {
+    res.status(500).json({
+      status: 'failed',
+      msg: 'Something went wrong',
+      error: error?.message,
+    });
+  }
+}
+
+module.exports = { register, login, forgotPin };
